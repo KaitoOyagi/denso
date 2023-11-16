afa206a29f4c0f060f7ab5745dc686d13ef0206d
kaito <kaitooy205@gamil.com>
2023-11-16 14:27:43 +0900

    11/16

--- word.py
+++ word.py
@@ -9,134 +9,117 @@ import random
 from collections import Counter
 import boto3
 from botocore.exceptions import NoCredentialsError
-from flask import redirect, url_for
-
 
 matplotlib.use("Agg")
 
 
 def create_cloud(data):
-    # Google Apps ScriptのWebアプリケーションのURLを指定
-    web_app_url = "https://script.google.com/macros/s/AKfycbyGI_JTsJ_-Lo4tdmWSi3iXbw3CrxUvq2cKPZnGxHIbpKD_xZA5gNYxWuYA3iZHD9gjxQ/exec"
+    # valuesからテキストデータを抽出
+    text_data = " ".join(data)
 
-    # GETリクエストを送信してJSONデータを取得
-    response = requests.get(web_app_url)
+    # テキストデータをトークナイズ
+    t = Tokenizer()
+    tokens = t.tokenize(text_data)
 
-    # 応答をJSON形式として読み込む
-    if response.status_code == 200:
-        # JSONデータを取得
-        values = response.json()
+    word_list = []
+    # 単語の出現回数をカウント
+    word_count = Counter()
 
-        # valuesからテキストデータを抽出
-        text_data = ""
-        for row in values:
-            text_data += " ".join(row) + "\n"  # 各セルのテキストをスペース区切りで結合し、改行で区切る
+    for token in tokens:
+        word = token.surface
+        partOfSpeech = token.part_of_speech.split(",")[0]
+        partOfSpeech2 = token.part_of_speech.split(",")[1]
 
-        # テキストデータをトークナイズ
-        t = Tokenizer()
-        tokens = t.tokenize(text_data)
+        if partOfSpeech in ["名詞", "動詞", "形容詞", "形容動詞"]:
+            if partOfSpeech != "記号":
+                if (
+                    (partOfSpeech2 != "非自立")
+                    and (partOfSpeech2 != "代名詞")
+                    and (partOfSpeech2 != "数")
+                ):
+                    word_list.append(word)
+                    word_count[word] += 1  # 単語の出現回数をカウント
 
-        word_list = []
-        # 単語の出現回数をカウント
-        word_count = Counter()
+    # 単語の出現回数が1回のものを削除
+    word_list = [word for word in word_list if word_count[word] > 1]
 
-        for token in tokens:
-            word = token.surface
-            partOfSpeech = token.part_of_speech.split(",")[0]
-            partOfSpeech2 = token.part_of_speech.split(",")[1]
+    words = " ".join(word_list)
+    print(words)
+    stop_words = [
+        "デンソー",
+        "ある",
+        "有る",
+        "高い",
+        "問う",
+        "含む",
+        "同様な",
+        "幅広い",
+        "重んずる",
+        "クルマ",
+        "「",
+        "」",
+        "思います",
+        "です",
+        "ます",
+        "できる",
+        "こと",
+        "、",
+        "・",
+        "/",
+        "／",
+        '"',
+        "！",
+        "こと",
+        "し",
+        "あり",
+        "する",
+        "(",
+        ")",
+    ]
+    fpath = "./姫明朝しらゆきmini.otf"
 
-            if partOfSpeech in ["名詞", "動詞", "形容詞", "形容動詞"]:
-                if partOfSpeech != "記号":
-                    if (
-                        (partOfSpeech2 != "非自立")
-                        and (partOfSpeech2 != "代名詞")
-                        and (partOfSpeech2 != "数")
-                    ):
-                        word_list.append(word)
-                        word_count[word] += 1  # 単語の出現回数をカウント
+    color_palette = [
+        "#8246AF",
+        "#F08CAF",
+        "#DC0032",
+        "#FAB932",
+        "#1E9146",
+        "#0091BE",
+    ]
 
-        # 単語の出現回数が1回のものを削除
-        word_list = [word for word in word_list if word_count[word] > 1]
+    def custom_color(word, font_size, position, orientation, font_path, random_state):
+        return random.choice(color_palette)
 
-        words = " ".join(word_list)
-        print(words)
-        stop_words = [
-            "デンソー",
-            "ある",
-            "有る",
-            "高い",
-            "問う",
-            "含む",
-            "同様な",
-            "幅広い",
-            "重んずる",
-            "クルマ",
-            "「",
-            "」",
-            "思います",
-            "です",
-            "ます",
-            "できる",
-            "こと",
-            "、",
-            "・",
-            "/",
-            "／",
-            '"',
-            "！",
-            "こと",
-            "し",
-            "あり",
-            "する",
-            "(",
-            ")",
-        ]
-        fpath = "./姫明朝しらゆきmini.otf"
+    wordcloud = WordCloud(
+        font_path=fpath,  # フォントを指定しない
+        width=900,
+        height=600,
+        background_color="white",
+        stopwords=set(stop_words),
+        max_words=70,
+        min_font_size=4,
+        collocations=False,
+        color_func=custom_color,
+    ).generate(words)
 
-        color_palette = [
-            "#8246AF",
-            "#F08CAF",
-            "#DC0032",
-            "#FAB932",
-            "#1E9146",
-            "#0091BE",
-        ]
+    # ファイルに保存
+    local_output_image_path = "./static/image/wordcloud.png"
+    wordcloud.to_file(local_output_image_path)
 
-        def custom_color(
-            word, font_size, position, orientation, font_path, random_state
-        ):
-            return random.choice(color_palette)
+    # S3に画像をアップロード
+    s3_bucket_name = "wordcloud--bucket"  # Replace with your S3 bucket name
+    s3_object_key = "result/wordcloud.png"  # Specify the S3 object key after upload
+    try:
+        upload_to_s3(local_output_image_path, s3_bucket_name, s3_object_key)
 
-        wordcloud = WordCloud(
-            font_path=fpath,  # フォントを指定しない
-            width=900,
-            height=600,
-            background_color="white",
-            stopwords=set(stop_words),
-            max_words=70,
-            min_font_size=4,
-            collocations=False,
-            color_func=custom_color,
-        ).generate(words)
+        # S3上の画像のURLを返す
+        s3_image_url = f"https://{s3_bucket_name}.s3.amazonaws.com/{s3_object_key}"
+        print(f"S3 Image URL: {s3_image_url}")  # 追加: S3上の画像のURLをログに出力
+        return s3_image_url
 
-        # ファイルに保存
-        local_output_image_path = "./static/image/wordcloud.png"
-        wordcloud.to_file(local_output_image_path)
-
-        # S3に画像をアップロード
-        s3_bucket_name = "wordcloud--bucket"  # Replace with your S3 bucket name
-        s3_object_key = "result/wordcloud.png"  # Specify the S3 object key after upload
-        try:
-            upload_to_s3(local_output_image_path, s3_bucket_name, s3_object_key)
-
-            # S3上の画像のURLを返す
-            s3_image_url = f"https://{s3_bucket_name}.s3.amazonaws.com/{s3_object_key}"
-            print(f"S3 Image URL: {s3_image_url}")  # 追加: S3上の画像のURLをログに出力
-            return s3_image_url
-
-        except Exception as e:
-            print(f"Error uploading to S3: {str(e)}")  # 追加: S3へのアップロード時のエラーをログに出力
-            raise e
+    except Exception as e:
+        print(f"Error uploading to S3: {str(e)}")  # 追加: S3へのアップロード時のエラーをログに出力
+        raise e
 
 
 def upload_to_s3(local_file, bucket, s3_key, content_type="image/png"):
